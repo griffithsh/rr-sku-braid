d04aa1e5214d99e52283646be954bd3417de4709
Huw Griffiths <hgriffiths@seek.com.au>
2021-11-03 20:14:05 +1100

    Save a pristine sku init state

---
+++ sku.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  clientEntry: 'src/client.tsx',
+  renderEntry: 'src/render.tsx',
+  environments: ['development', 'production'],
+  publicPath: '/path/to/public/assets/', // <-- Required for sku build output
+  orderImports: true,
+};
